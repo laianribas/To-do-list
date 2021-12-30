cb6e9275a810fdf7e09ae27f3c09cda14f60084b
laianribas <laian67a@gmail.com>
2021-12-30 12:13:57 -0300

    Header feito

---
+++ src/components/Card/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './style.scss'
+
+const Card = () => {
+  return (
+    <div class="card">
+      <div class="card-header">Featured</div>
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item">An item</li>
+        <li class="list-group-item">A second item</li>
+        <li class="list-group-item">A third item</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Card
---
+++ src/components/Header/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header class="p-3 bg-dark text-white">
+      <div class="container">
+        <div class="d-flex flex-wrap align-items-center justify-content-center">
+          <h1 class="nav-link px-2 text-white">To-do List</h1>
+        </div>
+      </div>
+    </header>
+  )
+}
+export default Header
